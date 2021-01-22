ef288b163366fe994fde76fd1e5d1d3f59556e19
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2021-01-22 05:08:22 -0600

    feat: Adds a way to create a new client directory project via terminal alias
    The index.js file runs a series of check and filesystem commands using fs, path, and colors to find, display, and color code console logs.

---
+++ index.js
@@ -0,0 +1,66 @@
+require('colors')
+const fs = require('fs')
+const path = require('path')
+
+const structure = {
+    "01_Assets": {
+        Photos: {},
+        Typography: {},
+        Copy: {},
+        Misc: {}
+    },
+    "02_Design": {},
+    "03_Code": {},
+    "04_Proofs": {},
+    "05_Deliverables": {},
+};
+
+const create = (dir, structure, cb=null) => {
+    cb = (cb => (...a) => setTimeout(() => cb.apply(null, a)))(cb);
+    const subdirs = Reflect.ownKeys(structure);
+  
+    if(subdirs.length){
+      const sub = subdirs[0];
+      const pth = path.join(dir, sub);
+      const subsub = structure[sub];
+      const copy = Object.assign({}, structure);
+      delete copy[sub];
+  
+      fs.mkdir(pth, err => {
+        if(err) return cb(err);
+        create(pth, subsub, err => {
+          if(err) return cb(err);
+          create(dir, copy, cb);
+        });
+      });
+    }else{
+      cb(null);
+    }
+  };
+
+const clientName = process.argv[3]
+
+if (!clientName) {
+    console.error("🚨  Please supply a valid client name  🚨".red )
+    process.exit(1)
+}
+
+const clientsDirectory = process.argv[2]
+
+console.log(`🕵️‍♂️  Checking for destination folder ${clientsDirectory}  🕵️‍♂️ \n`.cyan)
+
+if(!fs.existsSync(clientsDirectory)) {
+    console.error( `\n\n\n🚨  No such path available: ${clientsDirectory}  🚨`.red);
+    process.exit(1)
+} else {
+    console.log(`🛠  ⚙️  Creating a Client folder with the name: ${clientName}  ⚙️  🛠\n`.cyan)
+    fs.mkdirSync(`${clientsDirectory}/${clientName}`)
+}
+
+const destinationDir = `${clientsDirectory}/${clientName}`
+  
+  create(destinationDir, structure, err => {
+    if(err) console.log(err);
+    else console.log(`🎉  Successfully created the ${clientName} client folder and subfolders! 🎉`.green);
+  });
+  
